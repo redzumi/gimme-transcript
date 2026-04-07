52b1f0587d9f768f64ac48aaca02bc3a96d5dffb
Evgenii Bykovskikh <redzumi.tao@gmail.com>
2026-04-07 21:43:36 +0300

    build: add recording window entry to electron-vite and tailwind config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,11 +4,28 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {},
-  preload: {},
+  preload: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/preload/index.ts'),
+          recording: resolve(__dirname, 'src/preload/recording.ts')
+        }
+      }
+    }
+  },
   renderer: {
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
+      }
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/renderer/index.html'),
+          recording: resolve(__dirname, 'src/renderer/recording/index.html')
+        }
       }
     },
     plugins: [react()]
